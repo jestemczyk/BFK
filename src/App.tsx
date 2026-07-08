d0fb5288e29ebf818629157f8ba64d6a2ad1b33e
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-07-08 22:56:45 +0300

    feat: Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router";
+import { MainLayout } from "./pages/MainLayout";
+import { MainPage } from "./pages/MainPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/users" />
+        <Route path="/users/:userId" />
+        <Route path="/users/:userId/books/:bookId" />
+        <Route path="/users/:userId/articles/:articleId" />
+        <Route path="/books" />
+        <Route path="/articles" />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
